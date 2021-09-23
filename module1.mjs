ad235e993fab28563445d1015efc1309a09aff1e
Parvez <parvezimohammed@gmail.com>
2021-09-23 10:41:36 +0530

    First Commit of TextUtils

---
+++ module1.mjs
@@ -0,0 +1,10 @@
+const a = "Parvez";
+const b = "Arfain";
+const c = "Muskaan";
+const d = "Siraj";
+
+export default b;
+
+export {a};
+export {c};
+export {d};
---
+++ module2.mjs
@@ -0,0 +1,6 @@
+import something, {a,c,d}  from './module1.mjs';
+console.log(something);
+
+console.log(a);
+console.log(c);
+console.log(d);
